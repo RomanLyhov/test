9a4aba777ee55e487cd1499a930a470af97e8b4d
pivas pivasovich <you@example.com>
2025-04-09 11:33:41 +0500

    Initial commit: создание проекта генератора паролей

---
+++ text/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Console.WriteLine("Введите длину пароля:");
+                if (int.TryParse(Console.ReadLine(), out int length) && length > 0)
+                {
+                    string password = GeneratePassword(length);
+                    Console.WriteLine($"Сгенерированный пароль: {password}");
+                }
+                else
+                {
+                    Console.WriteLine("Пожалуйста, введите корректную длину пароля.");
+                }
+            
+        }
+    }
+}
